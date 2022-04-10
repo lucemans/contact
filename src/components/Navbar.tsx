43459753887f948641cad0ae42ced4e99c833370
Lucemans <luc@lucemans.nl>
2022-04-10 23:49:11 +0200

    Alter Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,48 +6,12 @@ const Wrapper = styled.div`
     justify-content: center;
 `;
 
-const Nav = styled.nav`
-    width: 900px;
-    max-width: 100vw;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem 0;
-    margin: 0 1rem 2rem 1rem;
-    border-bottom: 1px dotted #686868;
-`;
-
-const Links = styled.div`
-    display: flex;
-    gap: 0.5rem;
-`;
-
-const Link = styled.a`
-    color: var(--color-main);
-    text-decoration: none;
-    &:hover {
-        text-decoration: underline;
-    }
-`;
-
-const LinkDivider = styled.div`
-    :before {
-        content: '/';
-        color: #686868;
-    }
-`;
-
-const Img = styled.img`
-    height: 2em;
-    margin-top: -0.5rem;
-    margin-bottom: -0.5rem;
-    width: auto;
-`;
-
 export const NavBar = () => {
 
     return (
         <Wrapper>
-            <Nav>
+            <luc-header></luc-header>
+            {/* <Nav>
                 <Link href="/">
                     <Img src="https://avatars.githubusercontent.com/u/10339043?v=4" alt="Profile Picture" />
                 </Link>
@@ -58,7 +22,7 @@ export const NavBar = () => {
                     <LinkDivider />
                     <Link href="https://luc.contact/secret" target="_blank">secret</Link>
                 </Links>
-            </Nav>
+            </Nav> */}
         </Wrapper>
     )
 };
