590a6e23042db40097ad0121d1bbc247d238e783
Lucemans <luc@lucemans.nl>
2022-04-15 11:59:51 +0200

    Update Mobile Basic Information

--- src/sections/BasicInfo.tsx
+++ src/sections/BasicInfo.tsx
@@ -1,5 +1,7 @@
-import { UserPlus } from 'react-feather';
+import { UserPlus, Twitter, GitHub, Linkedin, Youtube } from 'react-feather';
 import styled from 'styled-components';
+import ethereum from 'url:../../assets/skills/ethereum.webp';
+import { useMediaQuery } from 'react-responsive';
 
 import { Column, Table } from '../components/Table';
 
@@ -18,6 +20,9 @@ const ProfilePicture = styled.img`
 `;
 
 const Properties = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
     a {
         color: var(--color-main);
         text-decoration: none;
@@ -56,7 +61,160 @@ const DColumn = styled(Column)`
     justify-content: space-between;
 `;
 
+export const Socials = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1.4rem;
+    align-self: center;
+    justify-self: flex-end;
+    justify-content: flex-start;
+    margin-top: auto;
+    flex-grow: 1;
+    align-items: flex-end;
+    margin-top: 1rem;
+    img {
+        height: 1.4rem;
+    }
+    > a {
+        &:hover {
+            transform: scale(1.1);
+        }
+    }
+`;
+
+const DesktopInfo = () => {
+    return (
+        <Properties>
+            {[
+                ['Name', 'Luc van Kampen'],
+                ['Bio', 'Create Epic Shit'],
+                [
+                    'Tel',
+                    <a href="tel:0031618925911" target="_blank">
+                        +31 618 925 911
+                    </a>,
+                ],
+                [
+                    'Github',
+                    <a href="https://github.com/lucemans" target="_blank">
+                        lucemans
+                    </a>,
+                ],
+                [
+                    'Twitter',
+                    <a href="https://twitter.com/lucemansnl" target="_blank">
+                        @lucemansnl
+                    </a>,
+                ],
+                [
+                    'Linkedin',
+                    <a href="https://linkedin.com/in/lucemans" target="_blank">
+                        Luc van Kampen
+                    </a>,
+                ],
+                [
+                    'Youtube',
+                    <a
+                        href="https://www.youtube.com/c/Lucemans"
+                        target="_blank"
+                    >
+                        c/lucemans
+                    </a>,
+                ],
+                ['Eth', 'lucemans.eth'],
+                [
+                    'PGP Key',
+                    <a
+                        href="https://luc.contact/public/lucemans.asc"
+                        target="_blank"
+                    >
+                        69F2 4A82 16AA E27D 2D2A
+                        <br />
+                        428C E6D8 1E8E EC87 269A
+                    </a>,
+                ],
+            ].map((a, index) => (
+                <KeyValue key={index}>
+                    <Key>{a.at(0)}</Key>:&nbsp;
+                    <Value>{a.at(1)}</Value>
+                </KeyValue>
+            ))}
+        </Properties>
+    );
+};
+
+const MobileInfo = () => {
+    return (
+        <>
+            <Properties>
+                {[
+                    ['Name', 'Luc van Kampen'],
+                    ['Bio', 'Create Epic Shit'],
+                    [
+                        'Tel',
+                        <a href="tel:0031618925911" target="_blank">
+                            +31 618 925 911
+                        </a>,
+                    ],
+                    [
+                        'PGP Key',
+                        <a
+                            href="https://luc.contact/public/lucemans.asc"
+                            target="_blank"
+                        >
+                            69F2 4A82 16AA E27D 2D2A
+                            <br />
+                            428C E6D8 1E8E EC87 269A
+                        </a>,
+                    ],
+                ].map((a, index) => (
+                    <KeyValue key={index}>
+                        <Key>{a.at(0)}</Key>:&nbsp;
+                        <Value>{a.at(1)}</Value>
+                    </KeyValue>
+                ))}
+                <Socials>
+                    {[
+                        {
+                            e: <Twitter />,
+                            a: 'https://twitter.com/lucemansnl',
+                            l: 'Twitter'
+                        },
+                        {
+                            e: <GitHub />,
+                            a: 'https://github.com/lucemans',
+                            l: 'Github',
+                        },
+                        {
+                            e: <Linkedin />,
+                            a: 'https://linkedin.com/in/lucemans',
+                            l: 'Linkedin',
+                        },
+                        {
+                            e: <Youtube />,
+                            a: 'https://www.youtube.com/c/Lucemans',
+                            l: 'Youtube',
+                        },
+                        {
+                            e: <img src={ethereum} />,
+                            a: 'https://etherscan.io/address/lucemans.eth',
+                            l: 'Ethereum Address',
+                        },
+                    ].map((v) => (
+                        <a href={v.a} target="_blank" aria-label={v.l}>
+                            {v.e}
+                        </a>
+                    ))}
+                </Socials>
+            </Properties>
+        </>
+    );
+};
+
 export const BasicInfo = () => {
+    const isDesktopOrMobile = useMediaQuery({ query: '(min-width: 900px)' });
+
     return (
         <Table header="Basic Info">
             {() => [
@@ -76,71 +234,7 @@ export const BasicInfo = () => {
                     </a>
                 </CColumn>,
                 <DColumn>
-                    <Properties>
-                        {[
-                            ['Name', 'Luc van Kampen'],
-                            ['Bio', 'Create Epic Shit'],
-                            [
-                                'Tel',
-                                <a href="tel:0031618925911" target="_blank">
-                                    +31 618 925 911
-                                </a>,
-                            ],
-                            [
-                                'Github',
-                                <a
-                                    href="https://github.com/lucemans"
-                                    target="_blank"
-                                >
-                                    lucemans
-                                </a>,
-                            ],
-                            [
-                                'Twitter',
-                                <a
-                                    href="https://twitter.com/lucemansnl"
-                                    target="_blank"
-                                >
-                                    @lucemansnl
-                                </a>,
-                            ],
-                            [
-                                'Linkedin',
-                                <a
-                                    href="https://linkedin.com/in/lucemans"
-                                    target="_blank"
-                                >
-                                    Luc van Kampen
-                                </a>,
-                            ],
-                            [
-                                'Youtube',
-                                <a
-                                href="https://www.youtube.com/c/Lucemans"
-                                target="_blank"
-                                >
-                                    c/lucemans
-                                </a>
-                            ],
-                            ['Eth', 'lucemans.eth'],
-                            [
-                                'PGP Key',
-                                <a
-                                    href="https://luc.contact/public/lucemans.asc"
-                                    target="_blank"
-                                >
-                                    69F2 4A82 16AA E27D 2D2A
-                                    <br />
-                                    428C E6D8 1E8E EC87 269A
-                                </a>,
-                            ],
-                        ].map((a, index) => (
-                            <KeyValue key={index}>
-                                <Key>{a.at(0)}</Key>:&nbsp;
-                                <Value>{a.at(1)}</Value>
-                            </KeyValue>
-                        ))}
-                    </Properties>
+                    {isDesktopOrMobile ? <DesktopInfo /> : <MobileInfo />}
                     <a href="/public/lucemans.vcf" aria-label="Add to Contacts">
                         <Icon mobile={false}>
                             <UserPlus />
